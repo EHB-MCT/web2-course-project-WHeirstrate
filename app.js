a4481338222c142b9fe557ca8df367ad7fd33446
WHeirstrate <wouter.heirstrate@student.ehb.be>
2020-11-03 20:09:47 +0100

    Load HTML

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now())
+    }
+};
+
+app.use(express.static('/', 'index.js'));
 app.get('/', (req, res) => {
-    res.send('Ewa');
+    res.sendFile('/', '/index.html');
 });
 
 app.listen(port, () => {

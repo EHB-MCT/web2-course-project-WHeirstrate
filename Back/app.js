850373e807d22626137cdbbbcb3d3900092288f2
WHeirstrate <wouter.heirstrate@student.ehb.be>
2020-11-12 17:54:11 +0100

    Added npms

--- Back/app.js
+++ Back/app.js
@@ -1,10 +1,19 @@
-'use strict';
+let db;
+
 //EXPRESS
 const express = require('express');
 const app = express();
-const router = express.router();
+const router = express.Router();
 const port = 3000;
 
+//DB
+const MongoClient = require('mongodb').MongoClient;
+const db_name = "Milestone2";
+const uri = `mongodb+srv://admin:admin@cluster0.en9gr.mongodb.net/${db_name}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true
+});
+//MIDDLEWARE
 const cors = require('cors');
 app.use(cors());
 
@@ -21,4 +30,11 @@ app.get('/boardgames', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
+    client.connect(err => {
+        if (err) {
+            throw err;
+        }
+        db = client.db(db_name);
+        console.log("connected to database", db_name);
+    });
 });

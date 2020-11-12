6e70c6b24c311dd66c00f6ad6c29201cfd205989
WHeirstrate <wouter.heirstrate@student.ehb.be>
2020-11-12 15:30:52 +0100

    Remove NODE.js

--- Back/app.js
+++ Back/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/api/getObjects', (req, res) => {
+    console.log(path.join(__dirname, '/boardgames.json'));
+    fs.readFile(path.join(__dirname, '/boardgames.json'), err => {
+        console.log('Error: ', err);
+    });
 });
 
 app.listen(port, () => {
